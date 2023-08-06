a36bfbefa3814cf3ae9e853736dd5d2469ab78d7
Philip Massouh <philipmassouh@gmail.com>
2023-08-06 16:26:44 -0700

    wip

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -1,10 +1,10 @@
-import asyncio
 from collections import deque
 from datetime import timedelta
+from functools import partial
 
 import discord
 import yt_dlp
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 from music_constants import FFMPEG_OPTS, YTDLP_OPTS
 
@@ -86,28 +86,31 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._queue = deque()
-        self.last_ctx = None
-        self.music_loop.start()
         self.voice_client = None
 
-    @tasks.loop(seconds=1)
-    async def music_loop(self):
-        if self._is_playing() or len(self._queue) == 0:
-            await asyncio.sleep(1)
+    @commands.command()
+    async def leave(self, ctx):
+        """
+        If the bot is connected, disconnects it from the channel.
+        """
+        if self.voice_client:
+            await self.voice_client.disconnect()
+            self.voice_client = None
         else:
-            song = self._queue.popleft()
-            await self._play(self.last_ctx, song)
+            await ctx.send("I'm not connected to a voice channel.")
 
-    @music_loop.before_loop
-    async def before_music_loop(self):
-        await self.bot.wait_until_ready()
+    @commands.command()
+    async def view_queue(self, ctx):
+        """
+        Prints the queue where the first line is the upcoming song.
+        """
+        await ctx.send("Queue:\n" + "\n".join(str(x) for x in self._queue))
 
     @commands.command()
     async def skip(self, ctx):
         """
-        Skip the currently playing item.
+        Skip the currently playing song.
         """
-        self.last_ctx = ctx
         if (vc := ctx.voice_client) is not None:
             vc.stop()
         else:
@@ -116,63 +119,45 @@ class Music(commands.Cog):
     @commands.command()
     async def play(self, ctx, url):
         """
-        Joins the call. Given a url, creates a song and moves it to the front of
+        Joins the call. Given a url, creates a song and moves it to the start of
         the queue. If a song is currently playing, it will be skipped.
         """
-        self.last_ctx = ctx
         if not self.bot.voice_clients:
             await self._join_if_summoner_connected(ctx)
 
         self._queue.insert(0, YoutubeSource.from_url(url, ctx.author.name))
         if self._is_playing():
             await self.skip(ctx)
+            await self._play_next(ctx)
 
     @commands.command()
     async def queue(self, ctx, url) -> None:
         """
-        Joins the call. Given a url, creates a song and adds it to the back of
+        Joins the call. Given a url, creates a song and adds it to the end of
         the queue.
         """
-        self.last_ctx = ctx
         if not self.bot.voice_clients:
             await self._join_if_summoner_connected(ctx)
         self._queue.append(YoutubeSource.from_url(url, ctx.author.name))
 
-    @commands.command()
-    async def view_queue(self, ctx):
-        """
-        Prints the queue where the first line is the upcoming song.
-        """
-        self.last_ctx = ctx
-        await ctx.send("Queue:\n" + "\n".join(str(x) for x in self._queue))
+        if not self._is_playing():
+            await self._play_next(ctx)
 
-    @commands.command()
-    async def leave(self, ctx):
-        """
-        If the bot is connected, disconnects it from the channel.
-        """
-        self.last_ctx = ctx
-        if self.voice_client:
-            await self.voice_client.disconnect()
-            self.voice_client = None
-        else:
-            await ctx.send("I'm not connected to a voice channel.")
-
-    async def _join_if_summoner_connected(self, ctx) -> bool:
-        if ctx.author.voice:
-            self.voice_client = await ctx.author.voice.channel.connect()
-            return True
-        return False
-
-    async def _play(self, ctx, song):
-        self.last_ctx = ctx
+    async def _play_next(self, ctx):
+        song = self._queue.popleft()
 
         if not self.bot.voice_clients:
             await self._join_if_summoner_connected(ctx)
 
         if self.voice_client:
             await ctx.send(embed=song.build_yt_embed())
-            self.voice_client.play(song)
+            self.voice_client.play(song, after=partial(self._play_next, ctx=ctx))
+
+    async def _join_if_summoner_connected(self, ctx) -> bool:
+        if ctx.author.voice:
+            self.voice_client = await ctx.author.voice.channel.connect()
+            return True
+        return False
 
     def _is_playing(self) -> bool:
         return self.voice_client is not None and self.voice_client.is_playing()
