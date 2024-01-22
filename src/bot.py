52b03e2e7d46325ab233d9abd06b0f7400698bb1
Philip Massouh <philipmassouh@gmail.com>
2024-01-22 01:02:51 -0800

    black, all working

--- src/bot.py
+++ src/bot.py
@@ -5,33 +5,30 @@ from discord.ext import commands, tasks
 import boto3
 from botocore.exceptions import ClientError
 import json
+
 # Silence useless bug reports messages
 youtube_dl.utils.bug_reports_message = lambda: ""
 
-def get_secret():
 
+def get_secret():
     secret_name = "dogbot_token_discord"
     region_name = "us-west-1"
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+    client = session.client(service_name="secretsmanager", region_name=region_name)
 
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
         raise e
 
-    secret = get_secret_value_response['SecretString']
+    secret = get_secret_value_response["SecretString"]
     secret = json.loads(secret)
     secret = secret["DOGBOT_TOKEN_DISCORD"]
 
     return secret
+
 
 class MyBot(commands.Bot):
     def __init__(self):
